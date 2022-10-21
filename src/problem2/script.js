7a0df69b55eff0221215269b2e41aabc48833312
anchengyang <anchengyang72@gmail.com>
2022-10-22 00:52:20 +0800

    first draft of problem 2

---
+++ src/problem2/script.js
@@ -0,0 +1,8 @@
+function openPopup(){
+  var popup = document.getElementById("popup");
+  popup.classList.add("open-popup");
+}
+function closePopup(){
+  var popup = document.getElementById("popup");
+    popup.classList.remove("open-popup");
+}
