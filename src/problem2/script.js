c1d87089b189544047b04c7533544620207e0a95
anchengyang <anchengyang72@gmail.com>
2022-10-22 18:51:06 +0800

    problem 3

--- src/problem2/script.js
+++ src/problem2/script.js
@@ -2,20 +2,7 @@ const addr = document.getElementById("input-address");
 const amount = document.getElementById("input-amount");
 const otp = document.getElementById("input-otp");
 const form = document.getElementById("form");
-const error = document.getElementById("error");
 
-// function validation(){
-//   if (addr.value === '' || amount.value==='' || otp.value===''){
-//     alert("Some fields were incomplete");
-//     return false;
-//   } else if (addr.value.substring(0,2) != "0x"){
-//     alert("Address must start with 0x");
-//     return false;
-//   } else {
-//     openPopup();
-//     return true;
-//   }
-// }
 form.addEventListener('submit', (e) =>{
   if (addr.value === '' || amount.value==='' || otp.value===''){
     e.preventDefault();
--- src/problem3/datasource.js
+++ src/problem3/datasource.js
@@ -1,1 +1,43 @@
-// your code here:
+class Datasource{
+    constructor(){
+        // empty constructor
+    }
+    async getPrices(){
+        /**
+         * @return: returns an array in the form of a promise
+         * 
+         * each element in the array represents 1 transaction
+         * each element is a dictionary
+         * each dictionary has 3 keys, pair, mid and quote
+         * 
+         * pair: returns the currency pair
+         * mid: returns an anonymous function to get the value between the buy and sell price
+         * quote: returns the quote currency of the trade
+         * 
+         */
+
+        // data is the prices data fetched from the https
+        // it is a promise
+        let data = await (await fetch("https://static.ngnrs.io/test/prices")).json();
+        // gets the value stored in prices, which is an array of different dictionaries, 1 for each transaction
+        data = data["data"]["prices"];
+        // returns a new array containing dictionaries that have pair, mid and quote as keys
+        return data.map(dict => {
+            return {"pair": dict["pair"], 
+                    "mid": () => (dict["buy"] + dict["sell"])/2, 
+                    "quote": () => dict["pair"].substring(3)
+                    }
+            });
+        
+    }
+}
+
+let ds = new Datasource();
+ds.getPrices()
+    .then(prices => {
+        prices.forEach(price => {
+            console.log(`Mid price for ${ price.pair } is ${ price.mid() } ${ price.quote() }.`);
+        });
+    }).catch(error => {
+        console.err(error);
+    });
